60b0a8c791b305964c2853ef53bd787e64081cc4
Styvensoft <steveen.echeverri@gmail.com>
2022-08-14 19:51:24 -0500

    First basic setup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const btnEncrypt = document.getElementById('btn_encrypt');
+const btnDecrypt = document.getElementById('btn_decrypt');
+
+const encrypt = () => {
+    console.log("Click encrypt");
+}
+
+const decrypt = () => {
+    console.log("Click decrypt");
+}
