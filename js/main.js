4b9266a8b7a2ccc5a97b1e1dfe4af1d1e6f2b204
Styvensoft <steveen.echeverri@gmail.com>
2022-08-16 23:20:31 -0500

    Created functions encrypt and decrypt

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
 const btnEncrypt = document.getElementById('btn_encrypt');
 const btnDecrypt = document.getElementById('btn_decrypt');
+const messageText = document.querySelector('.text__enter');
+
+const vowels = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+let stateEncry = false, stateDecry = false;
 
 const encrypt = () => {
-    console.log("Click encrypt");
+    stateEncry = true;
+    changeMessage(messageText.value);
 }
 
 const decrypt = () => {
-    console.log("Click decrypt");
+    stateDecry = true;
+    changeMessage(messageText.value);
 }
+
+const changeMessage = (message) => { 
+
+    console.log(stateEncry);
+    for (const i in vowels) {
+        
+        if (message.includes(vowels[i][0])) {
+            if (stateEncry) {
+                message = message.replaceAll(vowels[i][0], vowels[i][1]);   
+            }
+            if (stateDecry) {
+                message = message.replaceAll(vowels[i][1], vowels[i][0]);
+            }
+        }
+    }
+    stateEncry = false;
+    stateDecry = false;
+    //return message;
+
+    document.querySelector(".title__message").innerHTML = 'Recorded message';
+    document.querySelector(".message").innerHTML = message;
+}
+
+btnEncrypt.addEventListener("click", encrypt);
+btnDecrypt.addEventListener("click", decrypt);
