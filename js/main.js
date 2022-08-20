0a650a6d242aa1394e5ddec9b587ae0dab4d1d4f
Styvensoft <steveen.echeverri@gmail.com>
2022-08-19 23:48:25 -0500

    Created copy button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const btnEncrypt = document.getElementById('btn_encrypt');
 const btnDecrypt = document.getElementById('btn_decrypt');
+const btnCopy = document.getElementById('btn_copy');
 const messageText = document.querySelector('.text__enter');
+const output = document.querySelector('.message');
 
 const vowels = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
 let stateEncry = false, stateDecry = false;
@@ -14,10 +16,13 @@ const decrypt = () => {
     stateDecry = true;
     changeMessage(messageText.value);
 }
+const copy = () => {
+    navigator.clipboard.writeText(output.textContent);
+    alert('Copied text!')
+}
 
 const changeMessage = (message) => { 
 
-    console.log(stateEncry);
     for (const i in vowels) {
         
         if (message.includes(vowels[i][0])) {
@@ -33,9 +38,12 @@ const changeMessage = (message) => {
     stateDecry = false;
     //return message;
 
-    document.querySelector(".title__message").innerHTML = 'Recorded message';
+    document.querySelector(".img-dummy").style.display = 'none';
+    document.querySelector(".title__message").style.display = 'none';
+    document.querySelector(".btn-copy").style.display = 'block';
     document.querySelector(".message").innerHTML = message;
 }
 
 btnEncrypt.addEventListener("click", encrypt);
 btnDecrypt.addEventListener("click", decrypt);
+btnCopy.addEventListener("click", copy);
